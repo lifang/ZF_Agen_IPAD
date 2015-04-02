3669f6aaa593bec2a16d5611134cead856ef0974
MrHuanghanzhang <690612676@qq.com>
2015-04-02 10:06:05 +0800

    Initial Commit

---
+++ ZF_Agent_IPad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZF_Agent_IPad
+//
+//  Created by 黄含章 on 15/4/2.
+//  Copyright (c) 2015年 comdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZF_Agent_IPad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZF_Agent_IPad
+//
+//  Created by 黄含章 on 15/4/2.
+//  Copyright (c) 2015年 comdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
